6e5db559f1e6c36d3389eee27553282d434361f6
Bitmot GitHub <github@bitmot.com>
2026-07-20 11:59:08 -0700

    bitmot 0.2.33

--- Formula/io1d.rb
+++ Formula/io1d.rb
@@ -13,22 +13,22 @@ class Io1d < Formula
   on_macos do
     on_arm do
       url "https://bitmot.com/download/releases/0.2.33/bitmot-macos-arm64.tar.gz"
-      sha256 "b0bf532da71153a1f4036c25e647ea0ad74b6e2ae130c4d6fb21752aafe9a397"
+      sha256 "cb696bf8ed5d52666f0732e7c03dad36d33aa556fd8c14979cea8b7510756fc4"
     end
     on_intel do
       url "https://bitmot.com/download/releases/0.2.33/bitmot-macos-x86_64.tar.gz"
-      sha256 "2ca60feaa3c1aaa09e3d5160c0697e3ca1bfcc9bdf64752cb4d778ae82d3406d"
+      sha256 "2d51d7f9f7cae2f1f2ab8242668f537dd70e154933edb291c43448256833e85b"
     end
   end
 
   on_linux do
     on_arm do
       url "https://bitmot.com/download/releases/0.2.33/bitmot-linux-aarch64.tar.gz"
-      sha256 "cf6885d867f4a85188867fd429734c16fbea139de20a3952a5a194c3461bfc55"
+      sha256 "5ca1649272c5f3139482db15327686bf2552403bc1f547d507fd1f3d37b340be"
     end
     on_intel do
       url "https://bitmot.com/download/releases/0.2.33/bitmot-linux-x86_64.tar.gz"
-      sha256 "1353133e939f930018b8fa528d682b1d7c774f6c6dd19e72125af8cb2b05a697"
+      sha256 "bba6b4277fd7a54ede01ef2da5d1d0903823c9aea36f5b227582a0b1b0b314e2"
     end
   end
 
